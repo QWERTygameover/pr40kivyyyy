1f576b4e694e0db9b830794a0096740d592f59e5
sexbomb <arsbar91@gmail.com>
2024-06-16 17:44:39 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.spinner import Spinner
+from datetime import datetime
+
+
+class TimePickerApp(App):
+    def build(self):
+        self.layout = BoxLayout(orientation='vertical', padding=20)
+
+        # Спиннеры для выбора часов, минут и AM/PM
+        self.hour_spinner = Spinner(text='Часы', values=[str(i) for i in range(1, 13)])
+        self.minute_spinner = Spinner(text='Минуты', values=[f'{i:02}' for i in range(0, 60)])
+        self.ampm_spinner = Spinner(text='AM/PM', values=['AM', 'PM'])
+
+        # Кнопка для отображения выбранного времени
+        self.show_time_button = Button(text='Показать время', size_hint=(1, None), height=50)
+        self.show_time_button.bind(on_press=self.show_selected_time)
+
+        # Метка для отображения выбранного времени
+        self.selected_time_label = Label(text='Выбранное время: ', size_hint_y=None, height=50)
+
+        # Добавляем виджеты на экран
+        self.layout.add_widget(self.hour_spinner)
+        self.layout.add_widget(self.minute_spinner)
+        self.layout.add_widget(self.ampm_spinner)
+        self.layout.add_widget(self.show_time_button)
+        self.layout.add_widget(self.selected_time_label)
+
+        return self.layout
+
+    def show_selected_time(self, instance):
+        # Получаем выбранные значения из спиннеров
+        hour = self.hour_spinner.text
+        minute = self.minute_spinner.text
+        ampm = self.ampm_spinner.text
+
+        # Форматируем время
+        formatted_time = f'{hour}:{minute} {ampm}'
+
+        # Обновляем метку с выбранным временем
+        self.selected_time_label.text = f'Выбранное время: {formatted_time}'
+
+
+if __name__ == '__main__':
+    TimePickerApp().run()
